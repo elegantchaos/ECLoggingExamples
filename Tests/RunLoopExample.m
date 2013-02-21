26853f2ab1c998959b7f1816a26ccf85dd8ed657
Sam Deane <sam@elegantchaos.com>
2013-02-21 21:19:30 +0000

    iOS unit test examples

--- Tests/RunLoopExample.m
+++ Tests/RunLoopExample.m
@@ -1,10 +1,10 @@
 // --------------------------------------------------------------------------
-//! @author Sam Deane
-//
 //  Copyright 2013 Sam Deane, Elegant Chaos. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's 
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
+
+xx
 
 #import <ECLogging/ECParameterisedTestCase.h>
 
